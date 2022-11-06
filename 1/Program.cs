4d2c29cac672f326c364ca77046db7b9de8ce63f
«Nikolai» <>
2022-11-06 15:20:19 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿int numberN = new Random().Next(10000, 99999);
+Console.WriteLine($"Ввод пятизначного числа {numberN}");
+
+int result1 = (numberN / 10000) %10;
+int result2 = (numberN / 1000) % 10;
+int result4 = (numberN / 10) % 10;
+int result5 = numberN % 10;
+
+if (result5 == result1 && result2 == result4)
+{
+    Console.WriteLine($"Да, число {numberN} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Нет, число {numberN} не является палиндромом");
+}
+Footer
---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write($"Введите число ");
+
+int numIn = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+int BigNum = 0;
+while (num <= numIn)
+{
+    BigNum = num * num * num;
+    num++;
+    Console.WriteLine(BigNum);
+}
